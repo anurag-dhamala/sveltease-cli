6284107d65a334ca55622b89fb70402957e67651
Anurag Dhamala <anuragdhamala8@gmail.com>
2023-12-01 02:00:10 +0545

    update svelte cli

--- src/svelte-cli.ts
+++ src/svelte-cli.ts
@@ -1,34 +1,32 @@
 import chalk from "chalk";
 import { FIGLET_WELCOME_MSG, questions } from "./constants.js";
 import inquirer from "inquirer"
-import { handleTypeScript } from "./typescript-handler.js";
-import { handleTailwind } from "./tailwind-handler.js";
-import { checkIfViteProject } from "./vite-handler.js";
+import Store from "./Store.js";
+import {igniteFileGenerator, pkgInstaller} from "./file-utils.js";
 
 const showWelcomeMessage=()=> {
     const chalkObj = chalk.green(FIGLET_WELCOME_MSG)
     console.log(chalkObj)
-    console.log(chalk.green("Welcome to sveltease-cli. Please answer some questions \n"))
+    console.log(chalk.green("Welcome to sveltease. Please answer some questions \n"))
 }
 
-const collectQuestions=()=> {
+const collectQuestions =  ()=> {
     return inquirer.prompt(questions);
 }
 
-
-export const init = async () => {
+export const initSvelteaseSetup = async () => {
     console.clear();
     showWelcomeMessage();
-
     const answers = await collectQuestions();
-    
+    Store.pkgManager = answers.pkgManager;
+    Store.hasTypeScriptEnabled = answers.hasTypeScriptEnabled;
+    Store.isTailwindCssUsedInSvelte = answers.isTailwindCssUsedInSvelte;
     console.log(chalk.green("Please wait while we are setting you up...."))
+    pkgInstaller("sveltease").then(()=> {
+        igniteFileGenerator();
+    }).catch((err) => {
+        console.log(chalk.red("Something went wrong. Please check the error message below: "));
+        console.log(err);
+    });
 
-    const isViteProject: boolean = await checkIfViteProject();
-    if(!isViteProject) {
-        console.log(chalk.red("\nLooks like you are not in a vite project. Automatic configuration through sveltease-cli only supports vite for now. Please setup svelte in your project manually. Then you can use sveltease. Thankyou."))
-        process.exit(-1);
-    }
-    handleTypeScript(answers.hasTypescriptEnabled);
-    handleTailwind(answers.isTailwindCssUsedInSvelte);
 }
