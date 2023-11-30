bae20a1669d9a95a6e205bb128b995a885455b81
Anurag Dhamala <anuragdhamala8@gmail.com>
2023-12-01 02:00:25 +0545

    initial process optimization

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,43 @@
 import chalk from "chalk";
-import { init } from "./svelte-cli.js";
+import { initSvelteaseSetup } from "./svelte-cli.js";
+import { checkIfProjectDir } from "./file-utils.js";
+import {availableInitOptions} from "./constants.js";
+import * as process from "process";
+import Store from "./Store.js";
 
-const args = process.argv.slice(2);
 
-if(!(args.length === 1)) {
-    console.log(chalk.red("Invalid no. of arguments"));
-    process.exit(-1);
+
+if(!checkIfProjectDir()) {
+    console.log(chalk.red("Current directory doesn't contain package.json. Exiting..."))
+    process.exit(1);
 }
 
-init();
+const args = process.argv.slice(2);
+if(args.length !==2) {
+    console.log(chalk.red("Invalid no. of arguments."));
+    process.exit(1);
+}
+
+if(args[0] !=="init") {
+    console.log(
+        chalk.red("Invalid command."),
+        "\n",
+        chalk.greenBright("Try npx sveltease-cli init vite|next|webpack"),
+        "\n",
+        chalk.red("Exiting...")
+    );
+    process.exit(1);
+}
+
+if(!availableInitOptions.includes(args[1])) {
+    console.log(
+        chalk.red("Invalid command.\n"),
+        chalk.greenBright("Try npx sveltease-cli init vue | react-vite | react-cra | next\n"),
+        chalk.red("Exiting...")
+    );
+    process.exit(1);
+}
+
+Store.initOptions = args[1] as 'vue' | 'react-cra' | 'react-vite' | 'next';
+
+initSvelteaseSetup();
