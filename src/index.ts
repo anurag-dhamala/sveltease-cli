f011080a3c6c77b4c8d76bd464c3667e7f0213cd
Anurag Dhamala <anuragdhamala8@gmail.com>
2023-12-02 22:58:36 +0545

    add webpack check in cli

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,8 @@ if(!checkIfProjectDir()) {
 }
 
 const args = process.argv.slice(2);
-if(args.length !==2) {
+const isWebpackInit = args[1] && args[1] === 'webpack';
+if(!isWebpackInit && args.length !==2) {
     console.log(chalk.redBright("Invalid no. of arguments."));
     process.exit(1);
 }
@@ -35,7 +36,7 @@ if(args[0] !=="init") {
 if(!availableInitOptions.includes(args[1])) {
     console.log(
         chalk.redBright("Invalid command.\n"),
-        chalk.greenBright("Try npx sveltease-cli init vue | react-vite | react-cra | next\n"),
+        chalk.greenBright("Try npx sveltease-cli init vue | react-vite | react-cra | next | webpack\n"),
         chalk.redBright("Exiting.")
     );
     process.exit(1);
@@ -46,6 +47,13 @@ if(args[1] === "webpack" && !args[2]) {
         chalk.redBright("You must provide your webpack config file name along with webpack.\n"),
         chalk.greenBright("Try npx sveltease-cli init webpack webpack.config.js\n"),
         chalk.redBright("Exiting.")
+    );
+    process.exit(1);
+}
+
+if(args[1] === "webpack" && !Store.files.includes(args[2])) {
+    console.log(
+        chalk.redBright("The webpack file you provided doesn't exist. Exiting.")
     );
     process.exit(1);
 }
