28d9b248c69afac7b460907befb09e5185cbdcca
Anurag Dhamala <anuragdhamala8@gmail.com>
2023-12-02 22:57:56 +0545

    track webpack config type

--- src/Store.ts
+++ src/Store.ts
@@ -1,10 +1,11 @@
 export default class Store {
     static files: Array<string> = [];
     static initOptions: 'vue' | 'react-cra' | 'react-vite' | 'next' | 'webpack' = 'react-vite';
-    static isTailwindCssUsedInSvelte: boolean = false;
+    // static isTailwindCssUsedInSvelte: boolean = false;
     static pkgManager: String = "npm";
     static hasTypeScriptEnabled: boolean = true;
 
     // webpack specific
     static webpackFileName: string = "webpack.config.js";
+    static webpackWithCommonJs: boolean = false;
 }
